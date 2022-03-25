6f44f7719c9522c58c923c4cd05f282ef894eda8
jnMars <563438383@qq.com>
2022-03-25 12:01:29 +0800

    Fix - Guard repeat name bug

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WeCom",
+    name: "Guard-iOS-WeCom",
     defaultLocalization: "en",
     platforms: [
         .macOS(.v10_14), .iOS(.v10), .tvOS(.v13)
@@ -18,7 +18,7 @@ let package = Package(
         .binaryTarget(
             name: "WeCom",
             url: "https://github.com/Authing/guard-ios-wecom/releases/download/1.0.0/WeCom.xcframework.zip",
-            checksum: "e582a23437920a8015649dfade8e6a12936e0a9e2d7e0e0f3fd3489d1b91457a"
+            checksum: "a07d508e1a8b715089063a6f7c8f99b4b5af56eb47f75a4d16d029ea2402e712"
         )
     ]
 )
